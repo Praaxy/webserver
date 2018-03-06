84d299f4602af5487dfb68f3c1038a30e531e10e
VIVIES Antoine <antoine.vivies@epitech.eu>
2018-03-05 19:12:24 -0500

    docker-compose OK

--- www/index.php
+++ www/index.php
@@ -1,46 +1,36 @@
+<!DOCTYPE html>
 <html>
- <head>
-  <title>Hello...</title>
-
-  <meta charset="utf-8"> 
-
-  <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
-  <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.0/jquery.min.js"></script>
-  <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
-
-</head>
-<body>
-    <div class="container">
-    <?php echo "<h1>Hi! I'm happy</h1>"; ?>
-
-    <?php
-
-    // Connexion et sélection de la base
-    $conn = mysqli_connect('db', 'user', 'test', "myDb");
-
-
-    $query = 'SELECT * From Person';
-    $result = mysqli_query($conn, $query);
-
-    echo '<table class="table table-striped">';
-    echo '<thead><tr><th></th><th>id</th><th>name</th></tr></thead>';
-    while($value = $result->fetch_array(MYSQLI_ASSOC)){
-        echo '<tr>';
-        echo '<td><a href="#"><span class="glyphicon glyphicon-search"></span></a></td>';
-        foreach($value as $element){
-            echo '<td>' . $element . '</td>';
-        }
-
-        echo '</tr>';
-    }
-    echo '</table>';
-
-    /* Libération du jeu de résultats */
-    $result->close();
-
-    mysqli_close($conn);
-
-    ?>
-    </div>
-</body>
+    <head>
+    <title>JQuery Chat</title>
+    <style>
+        *{ margin: 0; padding: 0; box-sizing: border-box; }
+        body { font: 13px Helvetica, Arial; }
+        div { background: #000; padding: 3px; position: fixed; bottom: 0; width: 100%; }
+        div input { border: 0; padding: 10px; width: 90%; margin-right: .5%; }
+        div button { width: 9%; background: rgb(130, 224, 255); border: none; padding: 10px; }
+        #messages
+        { list-style-type: none; margin: 0; padding: 0; }
+        #messages li { padding: 5px 10px; }
+        #message li:nth-child(odd) { background: #eee; }
+    </style>
+    <script
+			  src="https://code.jquery.com/jquery-3.3.1.js"
+			  integrity="sha256-2Kok7MbOyxpgUVvAk/HJ2jigOSYS2auK4Pfzbm7uH60="
+			  crossorigin="anonymous"></script>
+    </head>
+    <body>
+        <ul id="messages"></ul>
+        <div>
+            <input id="w" autocomplete="off">
+            <button>Envoyer</button>
+        </div>
+    </body>
+    <script>
+        $("button").click(function() {
+            if($("#w").val() == '')
+                return;
+            $("#messages").append('<li>'+$("#w").val()+'</li>')
+            $("#w").val('')
+        })
+    </script>
 </html>

